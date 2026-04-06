29a8889fb73746641ba9e0eba584029663c705be
cyrus maina <cmaina2@my.centennialcollege.ca>
2026-04-06 01:03:54 -0400

    Backup: my version of App.jsx

---
+++ bitcourse-backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
+
+const app = express();
+const PORT = 8000;
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL, // React app URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+        credentials: true, // allow cookies
+    })
+);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello, welcome to the Classroom API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+app.use("/api/subjects", subjectsRouter);
